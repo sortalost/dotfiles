723962f65523dce11f202fdad661f2cb9789147d
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2024-11-28 18:14:21 +0530

    add wiki.py, use more vars

--- init.cmd
+++ init.cmd
@@ -1,12 +1,12 @@
 @echo off
 cls
 doskey home=cd %USERPROFILE%
-doskey cap=cd %USERPROFILE%\\Desktop\\yard\\cv2 ^&^& python nn.py $1 $2 $3 ^&^& cd %USERPROFILE%
-doskey welp=echo i will do something about welp. Welp!
+doskey welp=echo ascii, wiki, sf, np
 doskey sf = cd %USERPROFILE%\Desktop\play\Street_Fighter_2_Turbo ^&^& StreetFighter2Turbo.bat
-doskey ascii = python dotfiles/ascii.py
+doskey ascii = python %USERPROFILE%\dotfiles\ascii.py
+doskey wiki = python %USERPROFILE%\dotfiles\wiki.py $1 $2 $3 $4 $5 $6 $7 $8
 doskey np = notepad $1
 title %date%
 echo #==================================================================^>^>
-python dotfiles/ascii.py
-echo #------------------------[ %date% ]-----------------------------
+python %USERPROFILE%\dotfiles\ascii.py
+echo #-------------------------[ %date% ]-------------------------------
